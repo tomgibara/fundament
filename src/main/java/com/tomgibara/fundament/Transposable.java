230cccbbcc65cdb26fde0f8ba71b7ba77755557d
Tom Gibara <me@tomgibara.com>
2015-12-10 02:47:05 +0000

    Documents Transposable interface

--- src/main/java/com/tomgibara/fundament/Transposable.java
+++ src/main/java/com/tomgibara/fundament/Transposable.java
@@ -1,7 +1,23 @@
 package com.tomgibara.fundament;
 
+/**
+ * Exposes a method that allows the elements of an indexed collection to be
+ * swapped in-situ.
+ * 
+ * @author Tom Gibara
+ *
+ */
+
 public interface Transposable {
 
+	/**
+	 * Instructs the object to swap the elements at the designated positions.
+	 * In the case that <code>i == j</code> then no change should occur.
+	 * 
+	 * @param i the index of an item to be swapped
+	 * @param j the index of an item to be swapped
+	 */
+
 	void transpose(int i, int j);
-	
+
 }
